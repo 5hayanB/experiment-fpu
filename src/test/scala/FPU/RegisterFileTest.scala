0dc311efb8e731bcbf838059be258ea58cf196cb
5hayanB <baig.sh@proton.me>
2023-03-03 19:14:16 +0500

    updated testbench

--- src/test/scala/FPU/RegisterFileTest.scala
+++ src/test/scala/FPU/RegisterFileTest.scala
@@ -1,10 +1,11 @@
 package FPU
 
-import chisel3._, chiseltest._, org.scalatest.freespec.AnyFreeSpec
+import chisel3._, chiseltest._, org.scalatest.flatspec.AnyFlatSpec
 
 
-class RegisterFileTest extends AnyFreeSpec with ChiselScalatestTester {
-  "Register File" in {
+class RegisterFileTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "RegisterFile"
+  it should "run successfully" in {
     test(new RegisterFile) { regFile =>
       for (i <- 0 until 3) {
         regFile.io.rAddr(i).poke(i.U)

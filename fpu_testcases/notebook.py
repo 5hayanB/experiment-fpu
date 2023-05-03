d194222e97b5a31a97f53e55b4f66c5cacab8c56
5hayanB <baig.sh@proton.me>
2023-05-03 19:55:26 +0500

    remodeled directory structure

--- fpu_testcases/notebook.py
+++ fpu_testcases/notebook.py
@@ -1,14 +1,11 @@
+from bitstring import BitArray
 from icecream import ic
 from fcvt_w_s import bin_to_float
 
 
 def main() -> None:
-    a = 3032069376
-    ic(a)
-    ic(bin(a)[2:])
-    print(f'{a:032b}')
-    ic(f'{a:032b}')
-    ic(len(bin(a)[2:]))
+    a: BitArray = BitArray(float=float(2795684029), length=32)
+    ic(a.bin)
 
 
 if __name__ == '__main__':
--- src/main/scala/nucleusrv/components/fpu/FALU.scala
+++ src/main/scala/nucleusrv/components/fpu/FALU.scala
@@ -1,44 +1,52 @@
-//package nucleusrv.components.fpu
-//
-//import chisel3._, chisel3.util._
-//
-//
-//class FALU_IO extends Bundle {
-//  // Inputs
-//  val input    : Vec[UInt] = Input(Vec(3, UInt(32.W)))
-//  val aluCtl   : UInt      = Input(UInt(4.W))
-//  val roundMode: UInt      = Input(UInt(3.W))
-//
-//  // Outputs
-//  val out: UInt = Output(UInt(32.W))
-//}
-//
-//
-//class FALU extends Module {
-//  val io: FALU_IO = IO(new FALU_IO)
-//
-//  // Constants
-//  val expWidth: Int = 8
-//  val sigWidth: Int = 24
-//
-//  // Modules
-//
-//  // Debug Counter
-//  val counter: UInt = dontTouch(RegInit(0.U(32.W)))
-//  counter := counter + 1.U
-//
-//  // Connections
-//  Seq(
-//  ).map(
-//    f =>
-//      f._1 := f._2
-//  )
-//
-//  // Operation Selection
-//  io.out := MuxLookup(io.aluCtl, 0.U, Seq(
-//  ))
-//
-//
-//
-//  // DEBUG
-//}
+package nucleusrv.components.fpu
+
+import chisel3._, chisel3.util._
+import hardfloat._
+
+
+class FALU_IO extends Bundle {
+  // Inputs
+  val input    : Vec[UInt] = Input(Vec(3, UInt(32.W)))
+  val aluCtl   : UInt      = Input(UInt(4.W))
+  val roundMode: UInt      = Input(UInt(3.W))
+
+  // Outputs
+  val out: UInt = Output(UInt(32.W))
+}
+
+
+class FALU extends Module {
+  val io: FALU_IO = IO(new FALU_IO)
+
+  // Constants
+  val expWidth: Int = 8
+  val sigWidth: Int = 24
+
+  // Modules
+  val fadd = Module(new AddRecFN(expWidth, sigWidth))
+
+  // Debug Counter
+  val counter: UInt = dontTouch(RegInit(0.U(32.W)))
+  counter := counter + 1.U
+
+  // Connections
+  Seq(
+    (fadd.io.subOp, 0.B),
+    (fadd.io.a, recFNFromFN(expWidth, sigWidth, io.input(0))),
+    (fadd.io.b, recFNFromFN(expWidth, sigWidth, io.input(1))),
+    (fadd.io.roundingMode, io.roundMode),
+    (fadd.io.detectTininess, 1.B)
+  ).map(
+    f =>
+      f._1 := f._2
+  )
+
+  // Operation Selection
+  io.out := MuxLookup(io.aluCtl, 0.U, Seq(
+    12.U -> fNFromRecFN(expWidth, sigWidth, fadd.io.out)
+  ))
+
+
+
+  // DEBUG
+}
